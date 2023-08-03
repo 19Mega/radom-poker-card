fabb909a04d5eb1097d70c95e603ea07b6b424e8
Matias Borba <matiasborba89@gmail.com>
2023-08-03 14:32:31 -0300

    app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,58 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function start() {
+  /* 
+  <i class="bi bi-suit-heart-fill"></i>
+  <i class="bi bi-suit-diamond-fill"></i>
+  <i class="bi bi-suit-club-fill"></i>
+  <i class="bi bi-suit-spade-fill"></i>
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  text-black
+  text-danger
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  */
+  // let topIcon = document.getElementById("top-icon")
+  // console.log("1st- topIcon example: ",topIcon)
+
+  let iconsList = ["bi-suit-heart-fill text-danger","bi-suit-diamond-fill text-danger","bi-suit-club-fill text-black","bi-suit-spade-fill text-black"]
+  let numberList = ["1","2","3","4","5","6","7","8","9","10","J","Q","K"]
+  let randomIconNumber = Math.floor((Math.random()*iconsList.length))
+  let randomNumber = Math.floor((Math.random()*numberList.length))
+
+  // TOP ICON
+  let topIcon = document.querySelectorAll("i")[0]
+  topIcon.className = iconsList[randomIconNumber]
+  //console.log(">>>",topIcon[1]) // icono arriba
+  //console.log(">>>",topIcon[2]) // color
+
+  // BOTTOM ICON
+  let bottomIcon = document.querySelectorAll("i")[1]
+  bottomIcon.className = iconsList[randomIconNumber]
+  //console.log(">>>",bottomIcon[1]) // icono abajo
+  //console.log(">>>",bottomIcon[2]) // color
+ 
+  // CENTER ICON
+  let centerIcon = document.getElementById("center-icon")
+  centerIcon.innerHTML =  numberList[randomNumber]
+
+  let cardBorder = document.getElementById("card-border")
+
+  if (iconsList[randomIconNumber] === "bi-suit-heart-fill text-danger" || iconsList[randomIconNumber] === "bi-suit-diamond-fill text-danger") {
+    centerIcon.className = "text-danger"
+    cardBorder.className = "border rounded border-danger border-5 w-25 d-flex flex-column justify-content-between"
+    
+  }
+
+  console.log("probando >>", topIcon)
+  console.log(randomIconNumber)
+  console.log(randomNumber)
+
 };
+
+window.onload = start()
+
+function myFunction(){
+  location.reload(); 
+}
+
+
+document.getElementById("myButton").addEventListener("click", myFunction);
+
